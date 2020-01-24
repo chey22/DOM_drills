69b30d631041524a33d66aa39edcc943e8e7d048
chey22 <59664325+chey22@users.noreply.github.com>
2020-01-24 15:14:44 -0600

    Add files via upload

--- dom_dom_dom/dom_dom_dom.js
+++ dom_dom_dom/dom_dom_dom.js
@@ -1,30 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
     let button = document.createElement("button")
     let btnText = document.createTextNode("Add Square")
+    let counter = 0
+    let container = document.createElement('div')
+
+    container.className = 'container'
+    button.className = "button"
+    
     button.appendChild(btnText);
     document.body.appendChild(button)
-    button.className = "button"
+    document.body.appendChild(container)
 
-    
-    let d = 0
-    button.addEventListener("click", function() {
+    button.addEventListener("click", function () {
         let div = document.createElement("div")
         div.className = "boxes"
-        div.id = d
-        div.innerText = d
-        // let divText = document.createTextNode(`${d}`)
-        document.body.appendChild(div)
-        // div.appendChild(divText)
-        d++
+        div.id = counter
+        div.innerText = counter
+        container.appendChild(div)
+        counter++
+
         div.addEventListener("click", btnClicked)
-    });
-    
-    function btnClicked (e) {
-        element = e.target
-        element.parentNode.appendChild(element)
-    };
+        div.addEventListener("dblclick", btnDoubleClicked)
 
+        let colors = ["red", "blue", "orange", "green", "purple", "yellow", "pink"];
 
+        function getRandomColor() {
+            let getRandomColor = colors[Math.floor(Math.random() * colors.length)];
+            return getRandomColor;
+        }
 
+        function btnClicked(e) {
+            let randomColor = getRandomColor()
+            e.currentTarget.style.backgroundColor = randomColor
+        };
+
+        function btnDoubleClicked(e) {
+            if (div.id % 2 == 0) {
+                if (document.body.contains(div.nextSibling)) {
+                    div.nextSibling.remove()
+                } else {
+                    alert('There is no square after this')
+                }
+            } else {
+                if (document.body.contains(div.previousSibling)) {
+                    div.previousSibling.remove()
+                } else {
+                    alert('There is no square before this')
+                }
+
+            }
+        }
+    })
 
 });
